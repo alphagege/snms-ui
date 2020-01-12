34fa2e6c2bedebb4aa0f3150798bb46ee7319af1
秋水月云枫 <707530569@qq.com>
2020-01-12 19:55:26 +0800

    基本样式文件 vue-router vuex babel-pofile sass ele集成

--- src/main.js
+++ src/main.js
@@ -1,8 +1,23 @@
 import Vue from 'vue';
-import App from './App.vue';
 
+import 'normalize.css/normalize.css' // A modern alternative to CSS resets
+import '@babel/polyfill'
+
+import Element from 'element-ui'
+
+import '@/styles/index.scss' // global css
+import App from './App.vue';
+import router from './router'
+
+import store from './store'
+
+Vue.use(Element, {
+  size: 'mini' // set element-ui default size
+})
 Vue.config.productionTip = false;
 
 new Vue({
+  router,
+  store,
   render: h => h(App)
 }).$mount('#app');
