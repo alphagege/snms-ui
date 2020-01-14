da222389ac8cbb33fb163ea509e3059bc38908fc
秋水月云枫 <707530569@qq.com>
2020-01-14 09:37:56 +0800

    路由整合

--- src/components/Breadcrumb/index.vue
+++ src/components/Breadcrumb/index.vue
@@ -1,9 +1,12 @@
 <template>
   <el-breadcrumb class="app-breadcrumb" separator="/">
     <transition-group name="breadcrumb">
-      <el-breadcrumb-item v-for="(item,index) in levelList" :key="item.path">
-        <span v-if="item.redirect==='noRedirect'||index==levelList.length-1" class="no-redirect">{{ item.meta.title }}</span>
-        <a v-else @click.prevent="handleLink(item)">{{ item.meta.title }}</a>
+      <el-breadcrumb-item :key="item.path" v-for="(item,index) in levelList">
+        <span
+          class="no-redirect"
+          v-if="item.redirect==='noRedirect'||index==levelList.length-1"
+        >{{ item.meta.title }}</span>
+        <a @click.prevent="handleLink(item)" v-else>{{ item.meta.title }}</a>
       </el-breadcrumb-item>
     </transition-group>
   </el-breadcrumb>
--- src/router/index.js
+++ src/router/index.js
@@ -2,24 +2,25 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import Layout from '../views/Layout'
 import Admin from './modules/coframe'
-
-const _import = require('./_import_' + process.env.NODE_ENV);
 Vue.use(Router);
 
 export const constantRouterMap = [{
         path: '/login',
         name: 'login',
-        component: _import('login/index'),
+        component: () =>
+            import ('@/views/login/index.vue'),
         hidden: true
     },
     {
         path: '/404',
-        component: _import('errorPage/404'),
+        component: () =>
+            import ('@/views/errorPage/404.vue'),
         hidden: true
     },
     {
         path: '/401',
-        component: _import('errorPage/401'),
+        component: () =>
+            import ('@/views/errorPage/401.vue'),
         hidden: true
     }
 ]
@@ -30,7 +31,8 @@ export const asyncRouterMap = [{
         redirect: 'dashboard',
         children: [{
             path: 'dashboard',
-            component: _import('dashboard/index'),
+            component: () =>
+                import ('@/views/dashboard/index.vue'),
             name: 'dashboard',
             meta: {
                 title: '工作台',
